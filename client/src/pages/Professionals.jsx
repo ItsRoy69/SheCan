d606d60bad80d9d2476694798b4d42896ae9d38d
Suman Roy <95040233+Suman373@users.noreply.github.com>
2023-01-09 19:40:56 +0530

    fix: cards in professionals page

--- client/src/pages/Professionals.jsx
+++ client/src/pages/Professionals.jsx
@@ -2,59 +2,81 @@ import React from 'react';
 import { useState } from 'react';
 import '../styles/professionals.css';
 import axios from 'axios';
+import { useEffect } from 'react';
+import profCardImg from '../images/doc-card.jpg';
 
 const url = "https://shecan-production.up.railway.app";
 
 const Professionals = () => {
-    
+
     const [professionalsArr, setProfessionalsArr] = useState([]);
 
-    const handleSelection = async(e)=>{
-        e.preventDefault();
-        setProfessionalsArr([]);
-        // replace `` with url
-        const res = await axios.get(`http://localhost:8000/professional/get-professional-by-profession/${e.target.value}`);
-       if(res.status === 200){
-            setProfessionalsArr(res.data.Professionals);
-            // console.log(professionalsArr);
-       }else{
-           console.log("Error from server");
-       }
+    // const handleSelection = async(e)=>{
+    //     e.preventDefault();
+    //     setProfessionalsArr([]);
+    //     // replace `` with url
+    //     const res = await axios.get(`http://localhost:8000/professional/get-professional-by-profession/${e.target.value}`);
+    //    if(res.status === 200){
+    //         setProfessionalsArr(res.data.Professionals);
+    //         // console.log(professionalsArr);
+    //    }else{
+    //        console.log("Error from server");
+    //    }
+    // }
+
+    // fetch instructors
+    const fetchInstructors = async () => {
+        try {
+            const res = await axios.get("http://localhost:8000/professional/get-professional-by-profession/instructor");
+            if (res.status === 200) {
+                setProfessionalsArr(res.data.Professionals);
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
-    
+
+    useEffect(() => {
+        setProfessionalsArr([]);
+        fetchInstructors();
+    }, []);
+
     return (
         <div className='professionals-wrapper'>
             <h1>Get to know about the Professionals here</h1>
-            <p>Select the type of professionals you want to view </p>
+            <p>Below is the list of well renowned instructors who have joined us</p>
             <div className="professional-container">
-                <select
+                {/* <select
                 onChange={handleSelection} 
                 className="form-select">
                     <option value="" disabled selected>Choose profession</option>
                     <option value="instructor">Instructor</option>
                     <option value="pharmacist">Pharmacist</option>
                     <option value="gynaecologist">Gynaecologist</option>
-                </select>
-               <div className="professionals-flexbox">
-               {
-                    professionalsArr?.length>0 ?
-                    professionalsArr.map((item,index)=>(
-                        <>
-                        <div 
-                        className="professional-card"
-                        key={index}>
-                            <p className="name"><span>Name: </span>{item.name}</p>
-                            <p className="ph-no"><span>Phone: </span>{item.phone}</p>
-                            <p className="address"><span>Address: </span>{item.address}</p>
-                        </div>
-                        </>
-                    ))
-                    :
-                    <>
-                    <p className='nothing-to-show'>No professionals to show</p>
-                    </>
-                }
-               </div>
+                </select> */}
+                <div className="professionals-flexbox">
+                    {
+                        professionalsArr?.length > 0 ?
+                            professionalsArr.map((item, index) => (
+                                <>
+                                    <div
+                                        className="professional-card"
+                                        key={index}>
+                                        <img src={profCardImg} alt="card photo" />
+                                        <div className="details-container">
+                                            <p className="name"><span>Name: </span>{item.name}</p>
+                                            <p className="ph-no"><span>Phone: </span>{item.phone}</p>
+                                            <p className="address"><span>Address: </span>{item.address}</p>
+                                        </div>
+                                    </div>
+                                </>
+                            ))
+                            :
+                            <>
+                                <p className='nothing-to-show'>No professionals to show</p>
+                            </>
+                    }
+                </div>
             </div>
         </div>
     )
